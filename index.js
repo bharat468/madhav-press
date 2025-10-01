59de8bdd1db68c4eefadc59eead222b5ace61ba1
bharat <bharatpareek256@gmail.com>
2025-10-01 15:03:14 +0530

    new

---
+++ index.js
@@ -0,0 +1,12 @@
+let service = document.querySelector(".service")
+let About = document.querySelector(".About")
+let content = document.querySelector(".content")
+
+let menu = document.querySelector("#click")
+
+let menubox = document.querySelector(".newmenubox")
+
+
+menu.addEventListener("click", () => {
+    menubox.classList.toggle("active");
+});
