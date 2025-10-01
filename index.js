170a2aff706eb0963ad33d6cdfc50cdc8a40c84a
bharat <bharatpareek256@gmail.com>
2025-10-01 15:11:11 +0530

    new update

--- index.js
+++ index.js
@@ -2,11 +2,10 @@ let service = document.querySelector(".service")
 let About = document.querySelector(".About")
 let content = document.querySelector(".content")
 
-let menu = document.querySelector("#click")
-
-let menubox = document.querySelector(".newmenubox")
+let menu = document.querySelector("#click");
+let menubox = document.querySelector(".newmenubox");
 
 
 menu.addEventListener("click", () => {
-    menubox.classList.toggle("active");
+    menubox.classList.toggle("active");  
 });
